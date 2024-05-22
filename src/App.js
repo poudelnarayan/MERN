eecb30ae9c7cba00f26ad51564d4d02c822b48b3
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-23 02:32:58 +0545

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-function App() {
-  return <h1>React App</h1>;
-}
+import Users from "./user/pages/Users";
+import NewPlace from "./palces/pages/NewPlace";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
