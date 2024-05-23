23bbc2e59603e407be0c99e862e6006c2820acf7
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-23 13:11:24 +0545

    Adding main header

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -3,11 +3,15 @@ import "./UsersList.css";
 
 import UserItem from "./UserItem";
 
+import Card from "../../shared/components/UIElements/Card";
+
 const UserList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
