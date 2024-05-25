de0014796e7f17e0101bff67db8d34fa93669d9e
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-25 21:54:36 +0545

    error handling throuh middleware

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -37,9 +37,9 @@ router.get("/:pid", (req, res, next) => {
     return p.id === placeId;
   });
   if (!place) {
-    return res
-      .status(404)
-      .json({ message: "Could not find a place for the provided id." });
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+    throw error;
   }
   res.json({ place }); // => { place } => { place: place }
 });
@@ -50,9 +50,9 @@ router.get("/user/:uid", (req, res, next) => {
     return p.creator == userId;
   });
   if (!place) {
-    return res
-      .status(404)
-      .json({ message: "Could not find a place for the provided user id." });
+    const error = new Error("Could not find a place for the provided user id.");
+    error.code = 404;
+    return next(error);
   }
   res.json({ place });
 });
