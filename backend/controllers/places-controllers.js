4b0d0fbd6d674551f29252689c1614a84bddafcb
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-27 13:53:46 +0545

    creating and storing Documents in the database

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,13 +1,16 @@
 const uuid = require("uuid");
 const { validationResult } = require("express-validator");
-const httpError = require("../models/http-error");
+const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
+const mongo = require("mongoose");
+
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    description: "One of the most famous skyscrapers in the world!",
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/1024px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
     address: "20 W 34th St, New York, NY 10118, United States",
@@ -20,7 +23,7 @@ let DUMMY_PLACES = [
   {
     id: "p2",
     title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    description: "One of the most famous skyscrapers in the world!",
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/1024px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
     address: "20 W 34th St, New York, NY 10118, United States",
@@ -34,23 +37,21 @@ let DUMMY_PLACES = [
 
 const getPlaceById = (req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
   if (!place) {
-    throw new httpError("Could not find a place for the provided id.", 404);
+    return next(
+      new HttpError("Could not find a place for the provided id.", 404)
+    );
   }
-  res.json({ place }); // => { place } => { place: place }
+  res.json({ place });
 };
 
 const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter((p) => {
-    return p.creator == userId;
-  });
+  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
   if (!places || places.length === 0) {
     return next(
-      new httpError("Could not find a places for the provided user id.", 404)
+      new HttpError("Could not find places for the provided user id.", 404)
     );
   }
   res.json({ places });
@@ -59,51 +60,73 @@ const getPlacesByUserId = (req, res, next) => {
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    console.error("Validation errors:", errors.array());
     return next(
-      new httpError("Invalid inputs passed, please check your data.", 422)
+      new HttpError("Invalid inputs passed, please check your data.", 422)
     );
   }
+
   const { title, description, address, creator } = req.body;
+
   let coordinates;
   try {
-    coordinates = await getCoordsForAddress(req.body.address);
+    coordinates = await getCoordsForAddress(address);
+    console.log("Coordinates fetched successfully:", coordinates);
   } catch (error) {
-    return next(error);
+    console.error("Error fetching coordinates:", error);
+    return next(
+      new HttpError("Failed to fetch coordinates for the address.", 500)
+    );
   }
 
-  const createdPlace = {
-    id: uuid.v4(),
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    location: coordinates,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg/1024px-Empire_State_Building_from_the_Top_of_the_Rock.jpg",
     creator,
-  };
+  });
 
-  DUMMY_PLACES.push(createdPlace); // unshift(createdPlace) => add to the beginning
+  try {
+    await createdPlace.save();
+    console.log("Place saved successfully:", createdPlace);
+  } catch (err) {
+    console.error("Error saving place:", err);
+    return next(new HttpError("Creating place failed, please try again.", 500));
+  }
+
   res.status(201).json({ place: createdPlace });
 };
 
 const updatePlace = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new httpError("Invalid inputs passed, please check your data.", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
 
   const { title, description } = req.body;
   const placeId = req.params.pid;
-  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+
   const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
-  updatedPlace.title = title;
-  updatedPlace.description = description;
+  if (placeIndex === -1) {
+    return next(new HttpError("Could not find a place for that id.", 404));
+  }
+
+  const updatedPlace = { ...DUMMY_PLACES[placeIndex], title, description };
   DUMMY_PLACES[placeIndex] = updatedPlace;
+
   res.status(200).json({ place: updatedPlace });
 };
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
-  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
-    throw new httpError("Could not find a place for that id.", 404);
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  if (placeIndex === -1) {
+    return next(new HttpError("Could not find a place for that id.", 404));
   }
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
   res.status(200).json({ message: "Deleted place." });
