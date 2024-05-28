6371bc47be092299282c48738d5b4b33bfcd3cab
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-29 02:35:22 +0545

    more Auth COntext Usage

--- frontend/src/palces/components/PlaceItem.js
+++ frontend/src/palces/components/PlaceItem.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
 import "./PlaceItem.css";
 
+import { AuthContext } from "../../shared/context/auth-context";
+
 const PlaceItem = (props) => {
+  const auth = useContext(AuthContext);
+
   const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
@@ -71,10 +75,14 @@ const PlaceItem = (props) => {
             <Button inverse onClick={openMapHandler}>
               View on Map
             </Button>
-            <Button to={`/places/${props.id}`}>Edit</Button>
-            <Button danger onClick={showDeleteWarningHandler}>
-              Delete
-            </Button>
+            {auth.isLoggedIn && (
+              <Button to={`/places/${props.id}`}>Edit</Button>
+            )}
+            {auth.isLoggedIn && (
+              <Button danger onClick={showDeleteWarningHandler}>
+                Delete
+              </Button>
+            )}
           </div>
         </Card>
       </li>
--- frontend/src/shared/context/auth-context.js
+++ frontend/src/shared/context/auth-context.js
@@ -1,4 +1,4 @@
-import { CrerateContext, createContext } from "react";
+import { createContext } from "react";
 
 export const AuthContext = createContext({
   isLoggedIn: false,
