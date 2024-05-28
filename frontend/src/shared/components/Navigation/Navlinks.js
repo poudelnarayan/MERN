949cfcc6ceb6e6f6d2ee136af951fb44baea744a
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-29 02:22:55 +0545

    listening to context changtes

--- frontend/src/shared/components/Navigation/Navlinks.js
+++ frontend/src/shared/components/Navigation/Navlinks.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./Navlinks.css";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
+import { AuthContext } from "../../context/auth-context";
+
 const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
   return (
-    <ul className="nav-links" >
+    <ul className="nav-links">
       <li>
         <NavLink to="/" exact>
           All Users
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">My Places</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">Add Place</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">Authenticate</NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">My Places</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">Add Place</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">Authenticate</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
