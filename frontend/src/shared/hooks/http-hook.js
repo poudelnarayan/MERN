570f9356f5ceb8e2ffa91b6d93cd9aab9442f0a5
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-29 17:01:19 +0545

    Updating plces

--- frontend/src/shared/hooks/http-hook.js
+++ frontend/src/shared/hooks/http-hook.js
@@ -1,29 +1,20 @@
-import { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useCallback } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const activeHttpRequests = useRef([]);
-
   const sendRequest = useCallback(
     async (url, method = "GET", body = null, headers = {}) => {
       setIsLoading(true);
-      const httpAbortCtrl = new AbortController();
-      activeHttpRequests.current.push(httpAbortCtrl);
       try {
         const response = await fetch(url, {
           method,
           body,
           headers,
-          signal: httpAbortCtrl.signal,
         });
 
         const responseData = await response.json();
-
-        activeHttpRequests.current = activeHttpRequests.current.filter(
-          (reqCtrl) => reqCtrl !== httpAbortCtrl
-        );
 
         if (!response.ok) {
           throw new Error(responseData.message);
@@ -32,11 +23,6 @@ export const useHttpClient = () => {
         setIsLoading(false);
         return responseData;
       } catch (err) {
-        if (err.name === "AbortError") {
-          console.log("Fetch aborted");
-        } else {
-          setError(err.message || "Something went wrong!");
-        }
         setIsLoading(false);
         setError(err.message);
         throw err;
@@ -48,13 +34,6 @@ export const useHttpClient = () => {
   const clearError = () => {
     setError(null);
   };
-
-  useEffect(() => {
-    return () => {
-      console.log("Cleaning up activeHttpRequests");
-      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
-    };
-  }, []);
 
   return { isLoading, error, sendRequest, clearError };
 };
