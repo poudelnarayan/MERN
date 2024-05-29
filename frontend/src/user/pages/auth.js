1757c380efb9c1de441dee2adccc8c589fcad1da
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-29 20:39:53 +0545

    uploading images for new Places

--- frontend/src/user/pages/auth.js
+++ frontend/src/user/pages/auth.js
@@ -124,7 +124,12 @@ const Auth = () => {
             />
           )}
           {!isLoginMode && (
-            <ImageUpload center id="image" onInput={inputHandler} />
+            <ImageUpload
+              center
+              id="image"
+              onInput={inputHandler}
+              errorText="Please provide an image"
+            />
           )}
           <Input
             id="email"
