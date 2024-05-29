31eb7a743d816cb7315da6b3ba4dd86cfa2e23f6
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2024-05-29 19:47:46 +0545

    using imageUpload Component

--- frontend/src/user/pages/auth.js
+++ frontend/src/user/pages/auth.js
@@ -42,6 +42,7 @@ const Auth = () => {
         {
           ...formState.inputs,
           name: undefined,
+          image: undefined,
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -51,6 +52,10 @@ const Auth = () => {
           ...formState.inputs,
           name: {
             value: "",
+            isValid: false,
+          },
+          image: {
+            value: null,
             isValid: false,
           },
         },
@@ -63,6 +68,8 @@ const Auth = () => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
+
+    console.log(formState.inputs);
 
     if (isLoginMode) {
       try {
